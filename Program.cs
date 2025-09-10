0d162ccd8112b77469bc1e19aae4aeccfe3086ae
Jhonathan Mike <jhonathan.mke@gmail.com>
2025-09-10 20:33:56 -0300

    Adding Menu to help user during Operation

--- Program.cs
+++ Program.cs
@@ -1,10 +1,56 @@
-﻿using System.Text;
+﻿using System.Globalization;
+using System.Text;
 using DesafioProjetoHospedagem.Models;
 
 Console.OutputEncoding = Encoding.UTF8;
 
+int opcao = 1;
+do
+{
+    try
+    {
+        Console.Clear(); //TODO: Problema no VSCode durante o Debug | Está disparando Exception
+    }
+    catch
+    {
+        Console.WriteLine("");
+    }
+    Console.WriteLine("Bem Vindo ao nosso sistema de Hospedagem do Hotel C.NET");
+    Console.WriteLine("Por favor selecione a opção desejada: ");
+    Console.WriteLine("");
+    Console.WriteLine("1 - Listar Suítes");
+    Console.WriteLine("2 - Realizar Cadastro de Hóspedes");
+    Console.WriteLine("3 - Criar uma Nova Reserva");
+    Console.WriteLine("4 - Finalizar Reserva");
+    Console.WriteLine("Digite 0 para sair");
+    Console.WriteLine("");
+    Console.Write("Opção: ");
+
+    opcao = Convert.ToInt32(Console.ReadLine());
+
+    switch (opcao)
+    {
+        case 1:
+            //TODO: Lista todas as suites lidas do arquivo de configuração, mostrando o status: Reservada ou não.
+            break;
+        case 2:
+            //TODO: Realizar cadastro de Hóspede e persistir em arquivo, cadastro com identificador unico CPF.
+            break;
+        case 3:
+            //TODO: criar uma nova reserva solicitando CPF para cadastro de hospedes e solicitar seleção de suite.
+            break;
+        case 4:
+            //TODO: finalizar uma reserva, liberar suite e passar valor para o cliente
+            break;
+        default:
+            Console.WriteLine("Opção inválida!");
+            break;
+    }
+
+} while (opcao != 0);
+
 // Cria os modelos de hóspedes e cadastra na lista de hóspedes
-List<Pessoa> hospedes = new List<Pessoa>();
+List < Pessoa > hospedes = new List<Pessoa>();
 
 Pessoa p1 = new Pessoa(nome: "Hóspede 1");
 Pessoa p2 = new Pessoa(nome: "Hóspede 2");
